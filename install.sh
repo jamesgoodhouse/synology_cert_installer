469fe3a079f6bb76ab09a83e6de4a82993c688b6
James J. Goodhouse <j.goodhouse@gmail.com>
2021-01-28 23:49:11 -0800

    make more good

--- install.sh
+++ install.sh
@@ -3,13 +3,25 @@
 set -euf -o pipefail
 
 lego_certs_path=/etc/lego/certificates
+lego_cert_path="$lego_certs_path/$DOMAIN.crt"
+lego_issuer_cert_path="$lego_certs_path/$DOMAIN.issuer.crt"
+lego_key_path="$lego_certs_path/$DOMAIN.key"
+
 syno_certs_path=/usr/syno/etc/certificate/_archive
+syno_cert_path="$syno_certs_path/$CERT_IDENTIFIER/cert.pem"
+syno_key_path="$syno_certs_path/$CERT_IDENTIFIER/privkey.pem"
+syno_chain_path="$syno_certs_path/$CERT_IDENTIFIER/chain.pem"
+syno_fullchain_path="$syno_certs_path/$CERT_IDENTIFIER/fullchain.pem"
 
-cat "$lego_certs_path/$DOMAIN.crt" "$lego_certs_path/$DOMAIN.issuer.crt" > "$syno_certs_path/$CERT_IDENTIFIER/fullchain.pem"
-cp -f "$lego_certs_path/$DOMAIN.crt" "$syno_certs_path/$CERT_IDENTIFIER/cert.pem"
-cp -f "$lego_certs_path/$DOMAIN.key" "$syno_certs_path/$CERT_IDENTIFIER/privkey.pem"
-cp -f "$lego_certs_path/$DOMAIN.issuer.crt" "$syno_certs_path/$CERT_IDENTIFIER/chain.pem"
+if [ "$(diff "$lego_certs_path/$DOMAIN.crt" "$syno_cert_path")" != "" ]; then
+  echo "new certificate found"
+  cat "$lego_cert_path" "$lego_issuer_cert_path" > "$syno_fullchain_path"
+  cp -f "$lego_cert_path" "$syno_cert_path"
+  cp -f "$lego_key_path" "$syno_key_path"
+  cp -f "$lego_issuer_cert_path" "$syno_chain_path"
 
-chmod -R 400 "$syno_certs_path/$CERT_IDENTIFIER/"
+  chmod -R 400 "$syno_certs_path/$CERT_IDENTIFIER/"
 
-synoservice --restart nginx
+  echo "restarting web server"
+  synoservice --restart nginx
+fi
